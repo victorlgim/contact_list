442b4b9ee998a3e951425ad10105ab61f27282ff
victorglim <victor.guedes888@gmail.com>
2023-05-29 09:16:26 -0300

    feat: added contacts module with all endpoints crud generate

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
+import { ContactsModule } from './modules/contacts/contacts.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ContactsModule],
   controllers: [AppController],
   providers: [AppService],
 })
