a5e7ba3b3d3b962ea5b1b4ed66ad18b2fddaa6ec
victorglim <victor.guedes888@gmail.com>
2023-05-31 11:53:26 -0300

    feat: controller route /login @Post in auth modules

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, HttpCode } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
 
-@Controller('auth')
+@Controller('login')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(200)
+  @Post('')
+  @UseGuards(LocalAuthGuard)
+  async login(@Body() user: any) {
+    return this.authService.login(user.email);
+  }
 }
