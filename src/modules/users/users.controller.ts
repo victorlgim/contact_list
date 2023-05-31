d71ea094c98bbb257e2d314301bc375a4876c1d0
victorglim <victor.guedes888@gmail.com>
2023-05-31 11:31:25 -0300

    feat: use methods prisma service in controllers and services providers

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -13,17 +13,17 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  findOne(@Param('id') id: string, @Request() req: any) {
+    return this.usersService.findOne(id, req.user.id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto, @Request() req: any) {
+    return this.usersService.update(id, updateUserDto, req.user.id);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  remove(@Param('id') id: string, @Request() req: any) {
+    return this.usersService.remove(id, req.user.id);
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersRepository } from './repositories/users.repository';
@@ -8,27 +13,61 @@ import { NotFoundError } from 'rxjs';
 export class UsersService {
   constructor(private usersRepository: UsersRepository) {}
   async create(createUserDto: CreateUserDto) {
+    const findEmail = await this.usersRepository.findByEmail(
+      createUserDto.email,
+    );
+    if (findEmail) {
+      throw new ConflictException('E-mail already registered');
+    }
     const user = await this.usersRepository.create(createUserDto);
     return user;
   }
 
   async findByEmail(email: string) {
-    const user = await this.usersRepository.findOne(email);
+    const user = await this.usersRepository.findByEmail(email);
+    return user;
+  }
+
+  async findOne(id: string, idAuth: string) {
+    if (id !== idAuth) {
+      throw new ForbiddenException('Not authorized');
+    }
+    const user = await this.usersRepository.findOne(id);
     if (!user) {
-      throw new NotFoundException('User not found!');
+      throw new NotFoundException('User not found');
     }
     return user;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto, idAuth: string) {
+    const user = await this.usersRepository.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    if (id !== idAuth) {
+      throw new ForbiddenException('Not authorized');
+    }
+    if (updateUserDto.email) {
+      const findEmail = await this.usersRepository.findByEmail(
+        updateUserDto.email,
+      );
+      if (findEmail) {
+        throw new ConflictException('E-mail already registered');
+      }
+    }
+    const newUser = await this.usersRepository.update(id, updateUserDto);
+    return newUser;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string, idAuth: string) {
+    const user = await this.usersRepository.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    if (id !== idAuth) {
+      throw new ForbiddenException('Not authorized');
+    }
+    await this.usersRepository.delete(id);
+    return;
   }
 }
