299693a43ca46b5ef0a70626884085de6fa920dc
victorglim <victor.guedes888@gmail.com>
2023-05-31 11:12:04 -0300

    chore: alter config base main.ts implement transformOptions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ContactsModule } from './modules/contacts/contacts.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 
 @Module({
-  imports: [UsersModule, ContactsModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, ContactsModule, AuthModule]
 })
 export class AppModule {}
