922c00ee5575ac5e07a897b4a298fb01030f294e
victorglim <victor.guedes888@gmail.com>
2023-05-31 09:36:29 -0300

    feat: add guards jwt tokens validate with passport

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) { }
 
   async validateUser(userEmail: string, userPassword: string) {
     const user = await this.userService.findByEmail(userEmail);
@@ -18,5 +22,12 @@ export class AuthService {
       }
     }
     return null;
+  }
+
+  async login(email: string) {
+    const user = await this.userService.findByEmail(email);
+    return {
+      token: this.jwtService.sign({ email }, { subject: user.id }),
+    };
   }
 }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -12,11 +12,6 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
