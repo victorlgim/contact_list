5528867ab5b582af8dde9c48c675717b219dd977
victorlgim <109770689+victorlgim@users.noreply.github.com>
2023-07-27 11:46:24 -0300

    feat: update for deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
-import { ContactsModule } from './modules/contacts/contacts.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 
 @Module({
-  imports: [UsersModule, ContactsModule, AuthModule]
+  imports: [UsersModule, AuthModule]
 })
 export class AppModule {}
